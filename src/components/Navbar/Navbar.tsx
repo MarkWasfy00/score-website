49da98dae06b4985e943743e1b2035fd65da32b8
Mark Wasfy <76422492+MarkWasfy00@users.noreply.github.com>
2022-06-14 22:22:18 +0200

    rank up lightHouse

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
     <nav className={styles.nav}>
         <div className={`container ${styles.navHolder}`}>
-            <div className="slogan">
-                <img src="https://firebasestorage.googleapis.com/v0/b/score-website-1391b.appspot.com/o/webp%2FLogo.webp?alt=media&token=808b7a3e-acf3-4550-9a31-c634f7fc8ffc" alt="Logo" />
+            <div className={styles.slogan}>
+                <img src="https://firebasestorage.googleapis.com/v0/b/score-website-1391b.appspot.com/o/webp%2FLogo.webp?alt=media&token=808b7a3e-acf3-4550-9a31-c634f7fc8ffc" alt="Logo" width="100" height="100" />
             </div>
             <div className={`regular-25`}>
                 <ul className={styles.links}>
